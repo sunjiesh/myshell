4bb471e434798cd5b3ee8922d2c49b3340a9db40
Tom <sunjie310110@gmail.com>
2016-09-08 09:33:56 +0800

    git批量修改邮箱信息脚本

---
+++ git-author-rewrite-and-push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="oldemail@example.com"
+CORRECT_NAME="new name"
+CORRECT_EMAIL="newemail@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git push --force --tags origin 'refs/heads/*'
